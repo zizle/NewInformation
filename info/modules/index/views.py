832898f8209a25eaa7382c3e2acf17ab7acb8b1a
zizle <zizle_lin@163.com>
2018-05-28 14:20:17 +0800

    添加静态文件渲染主页

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,8 +1,15 @@
 # _*_ coding:utf-8 _*_
-
+from flask import render_template, current_app
 from . import index_bule
+
+
+@index_bule.route('/favicon.ico', methods=['GET'])
+def favicon():
+    """主页title小logo"""
+    return current_app.send_static_file('news/favicon.ico')
 
 
 @index_bule.route('/')
 def index():
-    return '<h1>hello flask!</h1>'
+    """主页"""
+    return render_template('index.html')
